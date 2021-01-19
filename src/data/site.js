3e29e27c723f2e6ae262f22d122297066bed301d
Usman Yunusov <usman.iunusov@gmail.com>
2021-01-19 12:18:47 +0300

    Change og image path

--- src/data/site.js
+++ src/data/site.js
@@ -1,11 +1,10 @@
 module.exports = {
-    'title': 'Фронтенд сегодня',
-    'subTitle': 'Блог про фронтенд и не только сегодня',
-    'description': 'Блог про фронтенд и не только сегодня',
-    'paths': {
-        'site': 'https://frontendtoday.ru',
-        'feed': 'https://frontendtoday.ru/articles/feed/',
-        'social': 'https://frontendtoday.ru/images/social.png'
-    },
-    'buildYear': new Date().getFullYear(),
+  title: 'Фронтенд сегодня',
+  subTitle: 'Блог про фронтенд и не только сегодня',
+  description: 'Блог про фронтенд и не только сегодня',
+  paths: {
+    site: 'https://frontendtoday.ru',
+    social: 'https://frontendtoday.ru/static/images/social.png',
+  },
+  buildYear: new Date().getFullYear(),
 };
